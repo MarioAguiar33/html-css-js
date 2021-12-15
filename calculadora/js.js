eeb28fb6f5b0e89fe28b2ac6144c1797fa2f08ea
Mario Aguiar <94297126+MarioAguiar33@users.noreply.github.com>
2021-12-15 11:55:34 -0300

    add js

--- calculadora/js.js
+++ calculadora/js.js
@@ -1,6 +1,11 @@
 function resetar(){
     document.getElementById("visor").innerHTML=""
 }
+function apagar(){
+    var resultado =document.getElementById("visor").innerHTML;
+    document.getElementById("visor").innerHTML=resultado.substring(0,resultado.length -1);
+
+}
 function inserir(num){
    var numero= document.getElementById("visor").innerHTML;
    document.getElementById("visor").innerHTML=numero+num;
