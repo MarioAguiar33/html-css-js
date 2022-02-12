3576ed4ff6f85782c7c4807f57bc67c5145c2b99
Mario Aguiar <marioaguiar33@outlook.com>
2022-02-12 11:47:29 -0300

    Update js.js

--- calculadora/js.js
+++ calculadora/js.js
@@ -1,6 +1,7 @@
 function resetar(){
     document.getElementById("visor").innerHTML=""
 }
+
 function apagar(){
     var resultado =document.getElementById("visor").innerHTML;
     document.getElementById("visor").innerHTML=resultado.substring(0,resultado.length -1);
