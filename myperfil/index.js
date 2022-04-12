fbc0d915d7a60bdee596cc4ff78b2287d49fe0b2
Mario Aguiar <marioaguiar33@outlook.com>
2022-04-12 13:06:52 -0300

    continuação do darkmode

--- myperfil/index.js
+++ myperfil/index.js
@@ -1,5 +1,18 @@
 const body=document.body;
+var imgdarkmode=document.querySelector('#darkmode');
+var dark = false;
+
 
 function darkMode(){
     body.classList.toggle('darkMode');
+    if (dark === true) {
+        imgdarkmode.src  = 'dark_mode_black_24dp.svg';
+    } else {
+       imgdarkmode.src = 'light_mode_white_24dp.svg';
+      
+    }
+    dark = !dark; 
+   
 }
+
+
