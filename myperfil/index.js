8bd8bf9c5998cb2b08de9f5b25d1bf513e6de88d
Mario Aguiar <marioaguiar33@outlook.com>
2022-04-13 11:15:44 -0300

    add skills

--- myperfil/index.js
+++ myperfil/index.js
@@ -3,6 +3,8 @@ const projects=document.querySelector('#projects');
 const skills=document.querySelector('#skills');
 const projetos=document.querySelector('#projetos');
 var imgdarkmode=document.querySelector('#darkmode');
+const divskills=document.querySelector('#showskills');
+
 var dark = false;
 
 
@@ -21,10 +23,12 @@ function darkMode(){
 function exibir(id){
     if(id==skills){
         projetos.style.display='none';
+        divskills.style.display='grid';
         id.style.backgroundColor='white';
         projects.style.backgroundColor='var(--cor2)';
 
     }else{
+        divskills.style.display='none';
         projetos.style.display='grid';
         id.style.backgroundColor='white';
         skills.style.backgroundColor='var(--cor2)';
