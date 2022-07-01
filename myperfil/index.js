4ba41150e1b0985b63351649eb2a793907cf683e
Mario Aguiar <marioaguiar33@outlook.com>
2022-07-01 15:56:39 -0300

    uptades

--- myperfil/index.js
+++ myperfil/index.js
@@ -2,23 +2,29 @@ const body=document.body;
 const projects=document.querySelector('#projects');
 const skills=document.querySelector('#skills');
 const projetos=document.querySelector('#projetos');
-var imgdarkmode=document.querySelector('#darkmode');
 const divskills=document.querySelector('#showskills');
 
-var dark = false;
+//DarkMode=====================================================
 
+
+imgToggleDarkMode.addEventListener('click', darkMode)
 
 function darkMode(){
+    const imgToggleDarkMode=document.querySelector('#imgToggleDarkMode');    
+    let dark = false;
+
     body.classList.toggle('darkMode');
     if (dark === true) {
-        imgdarkmode.src  = 'dark_mode_black_24dp.svg';
+        imgToggleDarkMode.src  = 'dark_mode_black_24dp.svg';
     } else {
-       imgdarkmode.src = 'light_mode_white_24dp.svg';
+        imgToggleDarkMode.src = 'light_mode_white_24dp.svg';
       
     }
     dark = !dark; 
    
 }
+
+//============================================================
 
 function exibir(id){
     if(id==skills){
